2d6bad201f1b0004ef664fdff6b0fa4f14c71653
jianzihao <wuyan@funtsui.com>
2019-07-08 10:43:06 +0700

    修改提交消息

---
+++ WYProgressHUD/hhshsdjfff的副本.h
@@ -0,0 +1,14 @@
+//
+//  hhshsdjfff.h
+//  podtext2
+//
+//  Created by wyman on 2018/5/3.
+//  Copyright © 2018年 wyman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+API_AVAILABLE(ios(2.0))
+@interface hhshsdjfff : NSObject
++(void)show;
+@end
---
+++ WYProgressHUD/hhshsdjfff的副本.m
@@ -0,0 +1,16 @@
+//
+//  hhshsdjfff.m
+//  podtext2
+//
+//  Created by wyman on 2018/5/3.
+//  Copyright © 2018年 wyman. All rights reserved.
+//
+
+#import "hhshsdjfff.h"
+
+@implementation hhshsdjfff
++(void)show{
+    NSLog(@"内猴");
+}
+
+@end
